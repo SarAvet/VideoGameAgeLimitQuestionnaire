5fd445a968d582e20d60765660290de82fdde0a8
Sargis.Avetisyan <Sargis.Avetisyan@KWSSARGISYAN>
2018-02-23 15:20:47 +0300

    questionaire view

--- VideoGameAgeLimitQuestionnaire.WEB/Controllers/HomeController.cs
+++ VideoGameAgeLimitQuestionnaire.WEB/Controllers/HomeController.cs
@@ -11,6 +11,11 @@ namespace VideoGameAgeLimitQuestionnaire.WEB.Controllers
             return View();
         }
 
+        public IActionResult Questionnaire()
+        {
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
